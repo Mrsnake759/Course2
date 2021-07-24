faa2cac49c3d4c52b8f23fbf25e601ada521928d
Artem <artem.sadomtsev@mail.ru>
2021-07-24 15:30:40 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// let number = 4;
+// const obj = {
+//     name: "John",
+//     age: 15,
+//     isMarried: false
+// };
+
+
+
+// let arr = [6, 4, 3, "Artem"];
+
+// console.log(arr);
+
+// alert('Hello user!');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+const answer = prompt("What is your name?", "John");
+console.log(typeof(answer));
